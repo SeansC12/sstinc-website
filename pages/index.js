45d45d82a620a9efdfde541032215def614e9b20
ye3tboi69 <87918915+ye3tboi69@users.noreply.github.com>
2022-04-15 20:56:16 +0800

    Installed firebase and added an example fetch

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,35 @@
-import Head from "next/head";
-import Image from "next/image";
+import { initializeApp } from "firebase/app";
+import { doc, getDoc, getFirestore } from "firebase/firestore/lite";
+import {useState} from 'react'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAe5UfatD1Z9jfpa7mnJV9mD-1l-j7RdE8",
+  authDomain: "sst-inc-website.firebaseapp.com",
+  databaseURL:
+    "https://sst-inc-website-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "sst-inc-website",
+  storageBucket: "sst-inc-website.appspot.com",
+  messagingSenderId: "224549318474",
+  appId: "1:224549318474:web:8534f293bcb7238f811bb3",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+
+
 
 export default function Home() {
+  const [data, setData] = useState(null)
+  async function getTestData() {
+    const docRef = doc(db, "attendance", "10-02-22");
+    const docSnap = await getDoc(docRef);
+    setData(docSnap.data()["Rick Astley"])
+  }
+  getTestData()
   return (
     <div>
       <h1 className="bg-lime-500">Hello World</h1>
-      <h2>Hello World 2</h2>
+      <h2>{data}</h2>
     </div>
   );
 }
