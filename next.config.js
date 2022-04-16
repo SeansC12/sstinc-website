6efcbe9d94b482c9791ea51a63c23de321233f14
SeansC12 <88256324+SeansC12@users.noreply.github.com>
2022-04-16 11:35:46 +0800

    Got the overall post stream structure using dummy data

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "cdn.pixabay.com",
+      "media.istockphoto.com",
+      "image.shutterstock.com",
+      "images.unsplash.com",
+      "thumbs.dreamstime.com",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { doc, getDoc, getFirestore } from "firebase/firestore/lite";
-import {useState} from 'react'
+import Image from "next/image";
+import { useState } from "react";
+import {
+  GoProject,
+  GoPulse,
+  GoPin,
+  GoPlay,
+  GoRepoClone,
+  GoRepo,
+} from "react-icons/go";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAe5UfatD1Z9jfpa7mnJV9mD-1l-j7RdE8",
@@ -14,22 +23,69 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app)
-
-
+const db = getFirestore(app);
+const dummyData = [
+  {
+    title: "Test 1",
+    desription: "This is the first update",
+    icon: GoProject,
+    imageLink:
+      "https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455__340.jpg",
+  },
+  {
+    title: "Test 2",
+    desription: "This is the second update",
+    icon: GoPulse,
+    imageLink:
+      "https://media.istockphoto.com/photos/hot-air-balloons-flying-over-the-botan-canyon-in-turkey-picture-id1297349747?b=1&k=20&m=1297349747&s=170667a&w=0&h=oH31fJty_4xWl_JQ4OIQWZKP8C6ji9Mz7L4XmEnbqRU=",
+  },
+  {
+    title: "Test 3",
+    desription: "This is the third update",
+    icon: GoPin,
+    imageLink:
+      "https://image.shutterstock.com/image-photo/mountains-under-mist-morning-amazing-260nw-1725825019.jpg",
+  },
+  {
+    title: "Test 4",
+    desription: "This is the fourth update",
+    icon: GoPlay,
+    imageLink:
+      "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&w=1000&q=80",
+  },
+  {
+    title: "Test 5",
+    desription: "This is the fifth update",
+    icon: GoRepoClone,
+    imageLink:
+      "https://thumbs.dreamstime.com/b/rainbow-love-heart-background-red-wood-60045149.jpg",
+  },
+];
 
 export default function Home() {
-  const [data, setData] = useState(null)
+  const [data, setData] = useState(null);
   async function getTestData() {
     const docRef = doc(db, "attendance", "10-02-22");
     const docSnap = await getDoc(docRef);
-    setData(docSnap.data()["Rick Astley"])
+    setData(docSnap.data()["Rick Astley"]);
   }
-  getTestData()
+  getTestData();
   return (
     <div>
-      <h1 className="bg-lime-500">Hello World</h1>
-      <h2>data from firebase: {data}</h2>
+      {dummyData.map((item) => (
+        <div class="p-6 mb-7 max-w-7xl mx-auto bg-white rounded-xl shadow-none flex items-center hover:shadow-lg cursor-pointer">
+          <div class="shrink-0">
+            {/* <item.icon></item.icon> */}
+            <Image src={item.imageLink} width={200} height={150}></Image>
+          </div>
+          <div class="pl-28 pt-2">
+            <div class="text-xl font-medium text-black break-words">
+              {item.title}
+            </div>
+            <p class="text-slate-500">{item.desription}</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
