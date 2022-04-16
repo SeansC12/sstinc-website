8870a0e8cbcf42d144e11f30731d29ecf7adcb00
y3etboi69 <harish_ram@s2021.ssts.edu.sg>
2022-04-16 15:53:06 +0800

    added firebase to newsfeed

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
 import { doc, getDoc, getFirestore } from "firebase/firestore/lite";
-import Image from "next/image";
 import { useState } from "react";
 import {
   GoProject,
@@ -24,68 +23,41 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const dummyData = [
-  {
-    title: "Test 1",
-    desription: "This is the first update",
-    icon: GoProject,
-    imageLink:
-      "https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455__340.jpg",
-  },
-  {
-    title: "Test 2",
-    desription: "This is the second update",
-    icon: GoPulse,
-    imageLink:
-      "https://media.istockphoto.com/photos/hot-air-balloons-flying-over-the-botan-canyon-in-turkey-picture-id1297349747?b=1&k=20&m=1297349747&s=170667a&w=0&h=oH31fJty_4xWl_JQ4OIQWZKP8C6ji9Mz7L4XmEnbqRU=",
-  },
-  {
-    title: "Test 3",
-    desription: "This is the third update",
-    icon: GoPin,
-    imageLink:
-      "https://image.shutterstock.com/image-photo/mountains-under-mist-morning-amazing-260nw-1725825019.jpg",
-  },
-  {
-    title: "Test 4",
-    desription: "This is the fourth update",
-    icon: GoPlay,
-    imageLink:
-      "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&w=1000&q=80",
-  },
-  {
-    title: "Test 5",
-    desription: "This is the fifth update",
-    icon: GoRepoClone,
-    imageLink:
-      "https://thumbs.dreamstime.com/b/rainbow-love-heart-background-red-wood-60045149.jpg",
-  },
-];
+
+const icons = {
+  GoProject: GoProject,
+  GoPulse: GoPulse,
+  GoPin: GoPin,
+  GoPlay: GoPlay,
+  GoRepoClone: GoRepoClone,
+  GoRepo: GoRepo,
+};
+
 
 export default function Home() {
-  const [data, setData] = useState(null);
+  const [allNews, setAllNews] = useState([]);
   async function getTestData() {
-    const docRef = doc(db, "attendance", "10-02-22");
+    const docRef = doc(db, "news", "AllNews");
     const docSnap = await getDoc(docRef);
-    setData(docSnap.data()["Rick Astley"]);
+    setAllNews(docSnap.data()["News"]);
+    console.log(docSnap.data()['News']);
   }
   getTestData();
+  return <div>{allNews.map((item) => NewsSection(item))}</div>;
+}
+
+function NewsSection(item) {
   return (
-    <div>
-      {dummyData.map((item) => (
-        <div class="p-6 mb-7 max-w-7xl mx-auto bg-white rounded-xl shadow-none flex items-center hover:shadow-lg cursor-pointer">
-          <div class="shrink-0">
-            {/* <item.icon></item.icon> */}
-            <Image src={item.imageLink} width={200} height={150}></Image>
-          </div>
-          <div class="pl-28 pt-2">
-            <div class="text-xl font-medium text-black break-words">
-              {item.title}
-            </div>
-            <p class="text-slate-500">{item.desription}</p>
-          </div>
+    <div class="p-6 mb-7 max-w-7xl mx-auto bg-white rounded-xl shadow-none flex items-center hover:shadow-lg cursor-pointer">
+      <div class="shrink-0">
+        <img src={item.imageLink} width={200} height={150}></img>
+      </div>
+      <div class="pl-28 pt-2">
+        <div class="text-xl font-medium text-black break-words">
+          {item.title}
         </div>
-      ))}
+        <p class="text-slate-500">{item.desription}</p>
+      </div>
     </div>
   );
 }
