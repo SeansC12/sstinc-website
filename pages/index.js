5c43430ecf8c1f126f7a10fa1a8ae7131232d2e7
y3etboi69 <harish_ram@s2021.ssts.edu.sg>
2022-04-18 16:04:53 +0800

    commented out firebase stuff

--- pages/index.js
+++ pages/index.js
@@ -11,16 +11,6 @@ import {
 } from "react-icons/go";
 import NewsSection from "./NewsSectionComponent";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAe5UfatD1Z9jfpa7mnJV9mD-1l-j7RdE8",
-  authDomain: "sst-inc-website.firebaseapp.com",
-  databaseURL:
-    "https://sst-inc-website-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "sst-inc-website",
-  storageBucket: "sst-inc-website.appspot.com",
-  messagingSenderId: "224549318474",
-  appId: "1:224549318474:web:8534f293bcb7238f811bb3",
-};
 const dummyData = [
   {
     title: "Test 1",
@@ -58,8 +48,19 @@ const dummyData = [
       "https://thumbs.dreamstime.com/b/rainbow-love-heart-background-red-wood-60045149.jpg",
   },
 ];
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAe5UfatD1Z9jfpa7mnJV9mD-1l-j7RdE8",
+//   authDomain: "sst-inc-website.firebaseapp.com",
+//   databaseURL:
+//     "https://sst-inc-website-default-rtdb.asia-southeast1.firebasedatabase.app",
+//   projectId: "sst-inc-website",
+//   storageBucket: "sst-inc-website.appspot.com",
+//   messagingSenderId: "224549318474",
+//   appId: "1:224549318474:web:8534f293bcb7238f811bb3",
+// };
+// const app = initializeApp(firebaseConfig);
+// const db = getFirestore(app);
 
 const icons = {
   GoProject: GoProject,
