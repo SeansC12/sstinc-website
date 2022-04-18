940c5846bcb56c58bec48078e2064cfc1121b1e1
Sairam-Suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2022-04-18 16:11:46 +0800

    Moved navbar to _app.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
 import { doc, getDoc, getFirestore } from "firebase/firestore/lite";
-import Image from "next/image";
 import { useState } from "react";
 import {
   GoProject,
@@ -77,25 +76,7 @@ export default function Home() {
       {dummyData.map((item) => (
         <PostStreamCard data={dummyData} />
       ))}
-      <Navbar bg="light" expand="lg" fixed={'top'}>
-        <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider/>
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+
     </div>
   );
 }
