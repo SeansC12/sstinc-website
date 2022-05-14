5e8d322ef46979a52a3f693054756f06b9105ec9
y3etboi69 <harish_ram@s2021.ssts.edu.sg>
2022-05-14 15:09:18 +0800

    finalised pastprojects and did homepage

--- pages/PastProjects.js
+++ pages/PastProjects.js
@@ -2,120 +2,97 @@ import React from "react";
 import Header from "../components/header";
 import { CodeIcon } from "@heroicons/react/solid";
 import { ParallaxBanner, ParallaxProvider } from "react-scroll-parallax";
+import { motion } from "framer-motion";
+import { useState } from "react";
+import { AnimatePresence } from "framer-motion";
+import { AnimateSharedLayout } from "framer-motion";
+import { initialiseApp } from "firebase/app";
+import { isMobile } from "react-device-detect";
+
+const randomImage = "https://picsum.photos/2000";
 
 const projects = [
   {
     title: "Tasks App",
     subtitle: "SwiftUI",
     description: "An App that helps people do action",
-    image: "https://picsum.photos/2004",
+    image: "https://picsum.photos/2000",
+    id: 0,
   },
   {
     title: "React Tracks",
     subtitle: "React and Python",
     description: "failure",
-    image: "https://picsum.photos/2003",
+    image: "https://picsum.photos/2000",
+    id: 1,
   },
   {
     title: "DevChat",
     subtitle: "React and Firebase",
     description: "more money => m1 pro",
-    image: "https://picsum.photos/2002",
+    image: "https://picsum.photos/2000",
+    id: 2,
   },
   {
     title: "Epic Todo App",
     subtitle: "React Hooks",
     description: "ooga booga",
-    image: "https://picsum.photos/2001",
+    image: "https://picsum.photos/2000",
+    id: 3,
   },
 ];
 
-function gallery() {
+// const isMobile = window.innerWidth < 1250
+
+function PastProjects() {
+  const [selectedId, setSelectedId] = useState(null);
   return (
     <ParallaxProvider>
       <Header tab="pastprojects" />
-      <main className="bg-blue-700">
+      <main>
         <ParallaxBanner
-          layers={[{ image: "https://picsum.photos/2000", speed: -50 }]}
-          className="aspect-[16/9] "
+          layers={[{ image: randomImage, speed: -50 }]}
+          className="h-screen"
         >
           <div className="absolute inset-0 flex items-center justify-center">
             <h1 className="text-8xl text-white font-thin">Our projects</h1>
           </div>
         </ParallaxBanner>
-        <PastPorjectSection />
+        <div className="h-screen items-center flex">
+          <div
+            className={
+              "w-screen justify-center items-center grid xl:grid-cols-2"
+            }
+          >
+            {projects.map((e) => Project(e))}
+          </div>
+        </div>
       </main>
     </ParallaxProvider>
   );
 }
 
-export default gallery;
-
-function PastPorjectSection() {
+function Project(info) {
+  const [expanded, setExpanded] = useState(false);
   return (
-    <div className="container px-5 py-10 mx-auto text-center lg:px-40 grow">
-      <div className="flex flex-col w-full mb-20">
-        <CodeIcon className="mx-auto inline-block w-10 mb-4" />
-        <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
-          Apps we've Built
-        </h1>
-        <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-          These are some of the many apps Inc has built for the community
-        </p>
-      </div>
-      <div className="flex flex-wrap -m-4">
-        {projects.map((project) => (
-          <a
-            href={project.link}
-            key={project.image}
-            className="sm:w-1/2 w-100 p-4"
-          >
-            <div className="flex relative">
-              <img
-                alt="gallery"
-                className="absolute inset-0 w-full h-full object-cover object-center"
-                src={project.image}
-              />
-              <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
-                <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
-                  {project.subtitle}
-                </h2>
-                <h1 className="title-font text-lg font-medium text-white mb-3">
-                  {project.title}
-                </h1>
-                <p className="leading-relaxed text-gray-400">
-                  {project.description}
-                </p>
-              </div>
-            </div>
-          </a>
-        ))}
+    <div
+      className={
+        "flex bg-opacity-50 h-60 border-4 m-14 rounded-xl justify-center place-items-center bg-[url('https://picsum.photos/2000')] hover:shadow-2xl"
+      }
+    >
+      <div className="grid place-items-center justify-center mx-6">
+        <p className="text-green-500 text-l xl:text-xl">{info.title}</p>
+        <p className="text-blue-700 text-xl xl:text-3xl">{info.description}</p>
+        <p className="text-black text-xl">{info.subtitle}</p>
       </div>
     </div>
+    // <div
+    // onClick={() => {setExpanded(!expanded)}}
+    //   className={
+    //     "h-48 w-screen justify-center place-items-center flex bg-[url('https://picsum.photos/2000')] hover:shadow-2xl"
+    //   }
+    // ></div>
   );
 }
 
-function topSection() {
-  return (
-    <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
-      <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-        <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-          At SST Inc,
-          <br className="hidden lg:inline-block" />
-          We love to build amazing apps.
-        </h1>
-        <p className="mb-8 leading-relaxed text-2xl">
-          SST Inc employees have made designed and implemented countless
-          applications for the community
-        </p>
-      </div>
-      <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-        <img
-          className="object-cover object-center rounded"
-          alt="hero"
-          src="https://picsum.photos/200"
-        />
-      </div>
-    </div>
-  );
-}
+export default PastProjects;
--- pages/index.js
+++ pages/index.js
@@ -1,57 +1,70 @@
 import { useState } from "react";
-import {
-  GoProject,
-  GoPulse,
-  GoPin,
-  GoPlay,
-  GoRepoClone,
-  GoRepo,
-} from "react-icons/go";
-import NewsSection from "./NewsSectionComponent";
+import PostStreamCard from "../components/PostStreamCard";
+import Header from "../components/header";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getDoc, collection, doc } from "firebase/firestore";
+import { a } from "react-spring";
+
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyANJ9zj5RXXvha1HAavXt-2300lK0Tyy1o",
+//   authDomain: "sstincwebsite.firebaseapp.com",
+//   projectId: "sstincwebsite",
+//   storageBucket: "sstincwebsite.appspot.com",
+//   messagingSenderId: "618289447481",
+//   appId: "1:618289447481:web:c8b32a37032453f5bd83e8",
+// };
+
+// const app = initializeApp(firebaseConfig);
+// const db = getFirestore(app)
 
 const dummyData = [
   {
     title: "Test 1",
     desription: "This is the first update",
-    imageLink:
+    image:
       "https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455__340.jpg",
   },
   {
     title: "Test 2",
     desription: "This is the second update",
-    imageLink:
+    image:
       "https://media.istockphoto.com/photos/hot-air-balloons-flying-over-the-botan-canyon-in-turkey-picture-id1297349747?b=1&k=20&m=1297349747&s=170667a&w=0&h=oH31fJty_4xWl_JQ4OIQWZKP8C6ji9Mz7L4XmEnbqRU=",
   },
   {
     title: "Test 3",
     desription: "This is the third update",
-    imageLink:
+    image:
       "https://image.shutterstock.com/image-photo/mountains-under-mist-morning-amazing-260nw-1725825019.jpg",
   },
   {
     title: "Test 4",
     desription: "This is the fourth update",
-    imageLink:
+    image:
       "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&w=1000&q=80",
   },
   {
     title: "Test 5",
     desription: "This is the fifth update",
-    imageLink:
+    image:
       "https://thumbs.dreamstime.com/b/rainbow-love-heart-background-red-wood-60045149.jpg",
   },
 ];
 
-const icons = {
-  GoProject: GoProject,
-  GoPulse: GoPulse,
-  GoPin: GoPin,
-  GoPlay: GoPlay,
-  GoRepoClone: GoRepoClone,
-  GoRepo: GoRepo,
-};
-
-
 export default function Home() {
-  return <div>{dummyData.map((item) => NewsSection(item))}</div>;
+  const [allNews, setAllNews] = useState([]);
+  // async function getNews() {
+  //   const r = await getDoc(doc(db, "News", "News"))
+  //   setAllNews(r.data().news)
+  // }
+  // getNews()
+  return (
+    <div>
+      <Header tab="Home" />
+      {dummyData.map((item) => ( //replace dummy data with allNews if firebase
+        <PostStreamCard className="px-4 py-6 sm:px-0" data={item} />
+      ))}
+    </div>
+  );
 }
