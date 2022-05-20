55a541cd40775e050ba19c9d6d3518b9f932c7f4
y3etboi69 <harish_ram@s2021.ssts.edu.sg>
2022-05-20 20:46:56 +0800

    minor changes

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,9 @@ import "../public/homeTopImg.jpeg";
 import "../public/homeBottomImg.jpeg";
 import "../public/im1.png";
 import "../public/projectSFIMg.jpeg";
+import Link from "next/link";
 
-function Home() {
+export default function Home() {
   return (
     <div>
       <Header tab="Home" />
@@ -17,15 +18,18 @@ function Home() {
           ></img>
           <div className="h-screen flex items-center justify-center relative z-20">
             <div>
-              <p className="text-3xl text-center w-screen my-2">
+              <p className="text-xl md:text-3xl text-center w-screen my-2">
                 Passionate about technology?
               </p>
-              <p className="text-2xl text-center w-screen">
+              <p className="text-m md:text-2xl text-center w-screen">
                 Join SST Inc today!
               </p>
               <div className="flex justify-center w-screen">
-                <button className=" h-10 bg-blue-500 opacity-100 rounded-full px-3 mt-5">
-                  <a href="https://forms.gle/ghnwWXnfrS1pXpUe7">Apply now</a>
+                <button className="h-10 bg-blue-500 opacity-100 rounded-full px-5 mt-5">
+                  <a href="https://forms.gle/ghnwWXnfrS1pXpUe7" target={"_blank"}>Apply now</a>
+                </button>
+                <button className="h-10 bg-blue-500 opacity-100 rounded-full px-5 mt-5 ml-5">
+                  <Link href="/AboutUs">Contact Us</Link>
                 </button>
               </div>
             </div>
@@ -34,11 +38,29 @@ function Home() {
 
         <div className="w-screen h-5 bg-white"></div>
 
-        <div>
+        <div className="items-center md:grid md:grid-cols-2 m-5">
+          <div className="justify-center text-center mr-5">
+            <p className="text-xl md:text-3xl">
+              See what we've done in the past
+            </p>
+            <p className="text-m">
+              At Inc, we thrive to help the community by making apps which
+              improve their lives blhahahah
+            </p>
+            <button className="h-10 bg-blue-500 opacity-100 rounded-full px-5 mt-3 mb-6">
+              <Link href="/PastProjects">Go</Link>
+            </button>
+          </div>
+          <div className="my-5">
+            <img className="rounded-3xl -mt-5" src="homeBottomImg.jpeg"></img>
+          </div>
+        </div>
+
+        {/* <div>
           <img
             className="h-screen object-cover w-screen opacity-30 absolute z-0"
             src="homeTopImg.jpeg"
-          ></img>
+          />
           <div className="h-screen flex items-center justify-center relative z-20">
             <div>
               <p className="text-2xl text-center w-screen my-2">
@@ -54,10 +76,8 @@ function Home() {
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
 }
-
-export default Home;
