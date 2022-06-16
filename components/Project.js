62b2cf63788578a13645be567a91aea499edcb9b
SeansC12 <88256324+SeansC12@users.noreply.github.com>
2022-06-16 16:52:02 +0800

    Finished desktop past projects, mobile view incomplete

--- components/Project.js
+++ components/Project.js
@@ -1,23 +1,139 @@
-import React from "react";
+import React, { useState } from "react";
+import { motion } from "framer-motion";
 
-function Project() {
-  const image = "https://picsum.photos/200";
-  const name = "Clicker App";
-  const description = "An app that counts how many times u clicked it";
-  const team = "Aurelius Yeo";
+function Project({ projectInfo }) {
+  const name = projectInfo.name;
+  const description = projectInfo.description;
+  const team = projectInfo.team;
+  const imageLink = projectInfo.imageLink;
+
+  let [isMouseHoveringOnProject, setIsMouseHoveringOnProject] = useState(false);
+
+  const projectDivVariant = {
+    mouseHovering: {
+      scale: 1,
+    },
+    mouseNotHovering: {
+      scale: 1,
+    },
+  };
+
+  const imageVariant = {
+    mouseHovering: {
+      scale: 0.95,
+      transition: {
+        type: "tween",
+        duration: 0.4,
+      },
+    },
+    mouseNotHovering: {
+      scale: 1,
+      transition: {
+        type: "tween",
+        duration: 0.4,
+      },
+    },
+  };
+
+  const textVariant = {
+    mouseHovering: {
+      x: 25,
+      originX: 0,
+      transition: {
+        type: "tween",
+        duration: "0.4",
+      },
+    },
+    mouseNotHovering: {
+      x: 0,
+      transition: {
+        type: "tween",
+        duration: "0.4",
+      },
+    },
+  };
+
+  const lineVariant = {
+    mouseHovering: {
+      scale: 1,
+      originX: 0,
+      transition: {
+        type: "tween",
+        duration: 0.4,
+      },
+    },
+    mouseNotHovering: {
+      scale: 0,
+      originY: 0,
+      transition: {
+        type: "tween",
+        duration: 0.4,
+      },
+    },
+  };
 
   return (
-    <div>
-      <img
+    <motion.div
+      initial="mouseNotHovering"
+      whileHover="mouseHovering"
+      variants={projectDivVariant}
+      onMouseEnter={() => {
+        setIsMouseHoveringOnProject(true);
+      }}
+      onMouseLeave={() => {
+        setIsMouseHoveringOnProject(false);
+      }}
+      onClick={() => {
+        setIsMouseHoveringOnProject(true);
+        isMouseHoveringOnProject.freeze();
+      }}
+      className="cursor-pointer"
+    >
+      <motion.img
+        initial={false}
+        animate={
+          isMouseHoveringOnProject ? "mouseHovering" : "mouseNotHovering"
+        }
+        variants={imageVariant}
         className="object-cover mb-10"
         width={580}
         height={594}
-        src={image}
+        src={imageLink}
       />
-      <h1 className="text-6xl mb-6">{name}</h1>
-      <h2 className="text-xl mb-2">{description}</h2>
-      <h3 className="text-lg text-gray-800 font-light">{team}</h3>
-    </div>
+      <h1 className="text-5xl mb-6">{name}</h1>
+      <div className="flex flex-row">
+        <motion.div
+          initial={false}
+          animate={
+            isMouseHoveringOnProject ? "mouseHovering" : "mouseNotHovering"
+          }
+          variants={lineVariant}
+          className="ml-5 border-l-[1px] h-[70px] border-white"
+        />
+        <div>
+          <motion.h2
+            initial={false}
+            animate={
+              isMouseHoveringOnProject ? "mouseHovering" : "mouseNotHovering"
+            }
+            variants={textVariant}
+            className="text-lg mb-2"
+          >
+            {description}
+          </motion.h2>
+          <motion.h3
+            initial={false}
+            animate={
+              isMouseHoveringOnProject ? "mouseHovering" : "mouseNotHovering"
+            }
+            variants={textVariant}
+            className="text-lg font-extralight"
+          >
+            {team}
+          </motion.h3>
+        </div>
+      </div>
+    </motion.div>
   );
 }
 
--- pages/PastProjects.js
+++ pages/PastProjects.js
@@ -14,6 +14,76 @@ function PastProjects() {
   let [reactUnderline, setReactUnderline] = useState(false);
   const reactRef = useRef();
 
+  const projects = {
+    iOS: {
+      ClickerApp: {
+        name: "Clicker App",
+        description: "An app that clicks",
+        team: "Aurelius Yeo",
+        imageLink: "https://picsum.photos/200",
+      },
+      ClickerApp2: {
+        name: "Clicker App2",
+        description: "An app that clicks 2",
+        team: "Aurelius Yeo",
+        imageLink: "https://picsum.photos/200",
+      },
+    },
+    Android: {
+      YouTube: {
+        name: "Youtube",
+        description: "Watch vids",
+        team: "Aurelius Yeo",
+        imageLink: "https://picsum.photos/200",
+      },
+      YouTube2: {
+        name: "Youtube2",
+        description: "Watch vids 2",
+        team: "Aurelius Yeo",
+        imageLink: "https://picsum.photos/200",
+      },
+    },
+    React: {
+      Website: {
+        name: "Website",
+        description: "Found on the web",
+        team: "Aurelius Yeo",
+        imageLink: "https://picsum.photos/200",
+      },
+      Website2: {
+        name: "Website2",
+        description: "Found on the web2",
+        team: "Aurelius Yeo",
+        imageLink: "https://picsum.photos/200",
+      },
+    },
+  };
+
+  const iosProjectComponents = [];
+  const androidProjectComponents = [];
+  const reactProjectComponents = [];
+
+  const keys = Object.keys(projects);
+  const ios = keys[0];
+  const android = keys[1];
+  const react = keys[2];
+
+  for (const project in projects[ios]) {
+    const projectInfo = projects[ios][project];
+    iosProjectComponents.push(<Project projectInfo={projectInfo} />);
+    console.log(projectInfo.name);
+  }
+  for (const project in projects[android]) {
+    androidProjectComponents.push(
+      <Project projectInfo={projects[android][project]} />
+    );
+  }
+  for (const project in projects[react]) {
+    reactProjectComponents.push(
+      <Project projectInfo={projects[react][project]} />
+    );
+  }
+
   const frameworkNameDivVariants = {
     hidden: {
       scale: 1,
@@ -64,7 +134,7 @@ function PastProjects() {
   };
 
   return (
-    <div className="relative">
+    <div className="bg-[#001220] text-white">
       <Header tab={"Past Projects"} />
       <div
         className={`bg-[#0d81fe] h-[60vh] w-full flex flex-col items-center justify-center text-white z-0 relative`}
@@ -81,11 +151,9 @@ function PastProjects() {
                 variants={frameworkNameDivVariants}
                 onMouseEnter={() => {
                   setIOSUnderline(true);
-                  console.log("Hovering");
                 }}
                 onMouseLeave={() => {
                   setIOSUnderline(false);
-                  console.log("Mouse left");
                 }}
                 onClick={executeIOSScroll}
               >
@@ -162,8 +230,10 @@ function PastProjects() {
           iOS App Development
         </h1>
         <div className="flex justify-around items-center">
-          <Project />
-          <Project />
+          {iosProjectComponents.map((project, key) => {
+            return <div key={key}>{project}</div>;
+            console.log(project);
+          })}
         </div>
       </div>
 
@@ -175,8 +245,10 @@ function PastProjects() {
           Android App Development
         </h1>
         <div className="flex justify-around items-center">
-          <Project />
-          <Project />
+          {androidProjectComponents.map((project, key) => {
+            return <div key={key}>{project}</div>;
+            console.log(project);
+          })}
         </div>
       </div>
       <div>
@@ -184,8 +256,10 @@ function PastProjects() {
           React JS
         </h1>
         <div className="flex justify-around items-center">
-          <Project />
-          <Project />
+          {reactProjectComponents.map((project, key) => {
+            return <div key={key}>{project}</div>;
+            console.log(project);
+          })}
         </div>
       </div>
     </div>
