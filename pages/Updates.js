6edf4282c8924cccaf481a0b6bcc5f827176d984
y3etboi69 <harish_ram@s2021.ssts.edu.sg>
2022-07-07 18:24:10 +0800

    almost done with supabase

--- pages/Updates.js
+++ pages/Updates.js
@@ -3,10 +3,24 @@ import Footer from "../components/Footer";
 import LargePostStreamCard from "../components/LargePostStreamCard";
 import SmallPostStreamCard from "../components/SmallPostStreamCard";
 import { createClient } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import Loader from "../components/Loader";
 
-const supabase = createClient("https://oegrlpmdgtysbebxhyue.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9lZ3JscG1kZ3R5c2JlYnhoeXVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTYzODYwODQsImV4cCI6MTk3MTk2MjA4NH0.3qjo3vnnJt3pTIZggw16RfUF5ZKUNmLdjZJHtxyIiuU", {
-  fetch: fetch.bind(globalThis)
-})
+const supabase = createClient(
+  "https://oegrlpmdgtysbebxhyue.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9lZ3JscG1kZ3R5c2JlYnhoeXVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTYzODYwODQsImV4cCI6MTk3MTk2MjA4NH0.3qjo3vnnJt3pTIZggw16RfUF5ZKUNmLdjZJHtxyIiuU",
+  {
+    fetch: fetch.bind(globalThis),
+  }
+);
+var data = 0;
+const getData = async () => {
+  console.log("getting data");
+  data = await supabase.from("updates").select("*");
+  console.log(data)
+  return data.body
+};
 
 const postStream = [
   {
@@ -49,23 +63,29 @@ const latestNews = {
 };
 
 export default function Home() {
+  const [updates, setUpdates] = useState(getData());
+
   return (
     <div className="">
       <Header tab="Updates" />
       <div className="flex items-center justify-center flex-col bg-[#001220] text-white pb-28">
         <div className="mt-8 mb-8 text-3xl">Latest News</div>
-        <div className="flex flex-col md:grid md:grid-cols-2 md:grid-rows-3 gap-8 m-auto mx-5 max-w-4xl">
-          <div className="md:col-start-1 md:col-span-2 md:row-start-1 md:row-span-1">
-            <LargePostStreamCard data={latestNews} />
+        {updates == undefined ? (
+          <Loader />
+        ) : (
+          <div className="flex flex-col md:grid md:grid-cols-2 md:grid-rows-3 gap-8 m-auto mx-5 max-w-4xl">
+            <div className="md:col-start-1 md:col-span-2 md:row-start-1 md:row-span-1">
+              <LargePostStreamCard data={latestNews} />
+            </div>
+            {updates.map((item, key) => (
+              <SmallPostStreamCard
+                key={key}
+                className="px-4 py-6 sm:px-0"
+                data={item}
+              />
+            ))}
           </div>
-          {postStream.map((item, key) => (
-            <SmallPostStreamCard
-              key={key}
-              className="px-4 py-6 sm:px-0"
-              data={item}
-            />
-          ))}
-        </div>
+        )}
       </div>
       <Footer />
     </div>
