cbe166486987f5029106118367356901e7e4795b
y3etboi69 <harish_ram@s2021.ssts.edu.sg>
2022-07-09 14:29:16 +0800

    removed supabase stuff, to prep for prod

--- pages/Updates.js
+++ pages/Updates.js
@@ -6,15 +6,6 @@ import { createClient } from "@supabase/supabase-js";
 import { useEffect, useState } from "react";
 import Loader from "../components/Loader";
 
-const supabase = createClient(
-  "https://oegrlpmdgtysbebxhyue.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9lZ3JscG1kZ3R5c2JlYnhoeXVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTYzODYwODQsImV4cCI6MTk3MTk2MjA4NH0.3qjo3vnnJt3pTIZggw16RfUF5ZKUNmLdjZJHtxyIiuU",
-  {
-    fetch: fetch.bind(globalThis),
-  }
-);
-
-
 const postStream = [
   {
     genre: "Social Media",
@@ -65,45 +56,24 @@ const latestNews = {
   date: "28 June, 2022",
 };
 
-var updates = []
-
-const getData = async () => {
-  console.log("getting data");
-  updates = await supabase.from("updates").select("*");
-  // return updates
-};
-
 export default function Home() {
-  const [updatesFromDB, setUpdates] = useState([]);
-  // setUpdates(getData())
-  getData()
-  useEffect(() => {
-    setUpdates(updates)
-  }, [updatesFromDB])
-  
-
-  
   return (
     <div>
       <Header tab="Updates" />
       <div className="flex items-center justify-center flex-col bg-[#001220] text-white pb-28">
         <div className="mt-8 mb-8 text-3xl">Latest News</div>
-        {updates == [] ? (
-          <Loader />
-        ) : (
-          <div className="flex flex-col md:grid md:grid-cols-2 md:grid-rows-3 gap-8 m-auto mx-5 max-w-4xl">
-            <div className="md:col-start-1 md:col-span-2 md:row-start-1 md:row-span-1">
-              <LargePostStreamCard data={latestNews} />
-            </div>
-            {updatesFromDB.map((item, key) => (
-              <SmallPostStreamCard
-                key={key}
-                className="px-4 py-6 sm:px-0"
-                data={item}
-              />
-            ))}
+        <div className="flex flex-col md:grid md:grid-cols-2 md:grid-rows-3 gap-8 m-auto mx-5 max-w-4xl">
+          <div className="md:col-start-1 md:col-span-2 md:row-start-1 md:row-span-1">
+            <LargePostStreamCard data={latestNews} />
           </div>
-        )}
+          {postStream.map((item, key) => (
+            <SmallPostStreamCard
+              key={key}
+              className="px-4 py-6 sm:px-0"
+              data={item}
+            />
+          ))}
+        </div>
       </div>
       <Footer />
     </div>
